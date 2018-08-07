002588cc5f818bb8f2d0ceae0d5ea01c13697837
Miłosz Panas <miloszpanas@gmail.com>
2018-08-07 17:28:51 +0200

    Implement functionality to display countries of a given continent

--- src/presentational/contact.component.js
+++ src/presentational/contact.component.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Contact = () => (
-    <h1>Street: Route 66</h1>
+    <div className="contactInfo">
+        <h1>Miłosz Panas</h1>
+        <h2><a href="mailto:miloszpanas@gmail.com">Send me a query</a></h2>
+        <a href="https://www.linkedin.com/in/milosz-panas-78459641/">or find me on LinkedIn</a>
+    </div>
 );
 
 export default Contact;
